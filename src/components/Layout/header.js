e2c57875b79b775eac22e2620504182ceb67b955
Stackbit Code Editor <projects@stackbit.com>
2021-03-24 01:30:36 +0000

    header.js: changed

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -36,7 +36,7 @@ class Header extends Component {
           <Link to="/about" className="sidebar-link">
             <button className="sidebar-item">ABOUT</button>
           </Link>
-          <Link to="https:merch.datab.ae" className="sidebar-link">
+          <Link to="https://merch.datab.ae" className="sidebar-link">
             <button className="sidebar-item">MERCH</button>
           </Link>
           <Link to="/contact" className="sidebar-link">
